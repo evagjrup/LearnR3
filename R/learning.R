e26f1d31157a915b57112aa59b2486160336d400
Eva Marie Gjørup <evagjoerup@clin.au.dk>
2023-04-25 15:38:47 +0200

    Comitting the learning file to history

---
+++ R/learning.R
@@ -0,0 +1,20 @@
+# Basis of R --------------------------------------------------------------
+
+colnames(airquality)
+head(airquality)
+str(airquality)
+summary(airquality)
+
+## If you wrote your code in a non-stylish matter, you can either highlight
+## your code and press cmd+shift+A or simply put the curser at the end of
+## the code, press cmd+shift+P and choose "style active file"
+
+1+1
+mean(2:6)
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
+
+r3::check_git_config()
+
