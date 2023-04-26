dba85a44a98596064342ac15c29f6311727ca460
Eva Marie Gjørup <evagjoerup@clin.au.dk>
2023-04-26 13:20:21 +0200

    Testing further

--- R/learning.R
+++ R/learning.R
@@ -66,24 +66,87 @@ nhanes_small %>%
 nhanes_small %>%
   select(bp_sys_ave, education)
 
-nhanes_small <- nhanes_small %>%
-  rename(bp_sys = bp_sys_ave,
-         bp_dia = bp_dia_ave)
+nhanes_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave
+  )
 
 nhanes_small %>%
-  select(bp_sys)
+  select(bp_sys_ave)
 
-# Rewrite
+## Rewrite
 select(nhanes_small, bmi, contains("age"))
 
 nhanes_small %>%
   select(bmi, contains("age"))
 
-# Rewrite to be more intuitive
+## Rewrite to be more intuitive
 blood_pressure <- select(nhanes_small, starts_with("bp_"))
-rename(blood_pressure, bp_systolic = bp_sys)
+rename(blood_pressure, bp_systolic = bp_sys_ave)
 
 nhanes_small %>%
   select(starts_with("bp_")) %>%
-  rename(bp_systolic = bp_sys) %>%
-  rename(bp_diastolic = bp_dia)
+  rename(bp_systolic = bp_sys_ave) %>%
+  rename(bp_diastolic = bp_dia_ave)
+
+
+#############################################
+# Filtering data by row
+## Only people who are NOT physically active
+nhanes_small %>%
+  filter(phys_active == "No")
+
+## Only people who ARE physically active
+nhanes_small %>%
+  filter(phys_active != "No")
+
+nhanes_small %>%
+  filter(phys_active == "Yes")
+
+## BMI = 25
+nhanes_small %>%
+  filter(bmi == 25)
+
+## Above or equal to 25
+nhanes_small %>%
+  filter(bmi >= 25)
+
+# Combining logical operators
+nhanes_small %>%
+  filter(bmi >= 25 & phys_active == "No")
+
+nhanes_small %>%
+  filter(bmi >= 25 | phys_active == "No")
+
+# Sorting / arranging data
+## Ascending
+nhanes_small %>%
+  arrange(age)
+
+## Descending
+nhanes_small %>%
+  arrange(desc(age))
+
+## Multiple columns
+nhanes_small %>%
+  arrange(education, age)
+
+# Transforming data, creating new columns
+## Adding columns with mutate
+
+## Figure out age in months (change age column, not new column)
+## rather than years
+nhanes_small %>%
+  mutate(age = age * 12)
+
+## Adding a new column with log-transformed BMI
+nhanes_small %>%
+  mutate(
+    age = age * 12,
+    logged_bmi = log(bmi)
+  )
+
+## Add a logical condition inside a function
+nhanes_small %>%
+  mutate(old = if_else(age >= 30, "Yes", "No"))
